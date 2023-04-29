44aaa1afe3118c0c1f75a49e71064f7749bd4c69
renjiePeng <1452304411@qq.com>
2023-04-29 11:36:30 +0800

    添加java并发编程实战的demo

--- java-concurrencyin-practice/src/main/java/com/prj/NoVisibility.java
+++ java-concurrencyin-practice/src/main/java/com/prj/NoVisibility.java
@@ -10,6 +10,20 @@ public class NoVisibility {
     private static boolean ready;
     private static int number;
 
+    public String name = "wew";
+
+    static {
+        System.out.println("加载父类……");
+    }
+
+    public String test1(String name){
+        return name+"father -test1";
+    }
+
+    public static String test2(String name){
+        return name+"father -test2";
+    }
+
     private static class ReaderThread extends Thread{
         @Override
         public void run() {
